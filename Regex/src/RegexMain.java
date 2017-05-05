c5f71c35b24e35687965bdbe399dc74cf9ba3af3
urtubeycarlos <urtubeycarlos.0510@outlook.com>
2017-05-04 21:34:46 -0300

    Expresiones regulares

--- Regex/src/RegexMain.java
+++ Regex/src/RegexMain.java
@@ -6,9 +6,8 @@ public class RegexMain {
 
 	public static void main (String[] args){
 		 //"foo" es la Regex // \\. es el punto . es el metacaracter [^prg] letras que no voy a considerar [a-z] un caracter de ese rango
-		Pattern pattern = Pattern.compile("\\w+[\\w\\.]+@\\w+(\\.\\w+)+"); 
-		Matcher matcher = pattern.matcher("Ahora con foo y foooo y refooooooomados"
-				+ " patri.bagnes@gmail.sarasa.com.ar manden");
+		Pattern pattern = Pattern.compile("0?[1-9]│[12][0-9]│3[01]"); 
+		Matcher matcher = pattern.matcher("numero de prueba 12 ");
 		
 		while (matcher.find()){ //Si encontro a foo dentro del String ingresado.
 			System.out.println("Encontre \"" + matcher.group() + "\" - ");

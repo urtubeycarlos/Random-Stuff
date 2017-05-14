93f27d640be80affcf3878c6a62512a24a35eb15
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2017-05-14 18:16:59 -0300

    First push: interface Grafo, class GrafoUnidireccional y class GrafoBidireccional.

---
+++ Grafo/src/grafo/Grafo.java
@@ -0,0 +1,15 @@
+package grafo;
+
+import java.util.Set;
+
+public interface Grafo<E> {
+
+	public boolean agregarVertice(E e);
+	public Set<E> getVertices();
+	public boolean agregarArista(E e1, E e2);
+	public boolean eliminarArista(E e1, E e2);
+	public boolean existeArista(E e1, E e2);
+	public Set<E> getVecinos(E e);
+	public int getGrado(E e);
+	
+}

a6ad6fb84342d54d15aee62f717e25f3c0731d01
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2017-05-14 21:19:30 -0300

    Mejorada la implementacion y agregados los tests

--- Matriz/src/matriz/Matriz.java
+++ Matriz/src/matriz/Matriz.java
@@ -1,78 +1,10 @@
 package matriz;
 
-import java.util.ArrayList;
+public interface Matriz<K, V> {
 
-public class Matriz<E> {
-
-	private ArrayList<ArrayList<E>> _m;
-	
-	public Matriz(int ancho, int alto){
-		_m = inicializarMatriz(ancho, alto);
-	}
-	
-	public E get(int fila, int columna){
-		checkearCoordenada(fila, columna);
-		return _m.get(fila).get(columna);
-	}
-	
-	public void set(E e, int fila, int columna){
-		checkearCoordenada(fila, columna);
-		_m.get(fila).set(columna, e);
-	}
-	
-	public int alto(){
-		return _m.size();
-	}
-	
-	public int ancho(){
-		return _m.get(0).size();
-	}
-	
-	public int dimension(){
-		return _m.size()*_m.get(0).size();
-	}
-	
-	public void redimensionar(int ancho, int alto){
-		
-		ArrayList<ArrayList<E>> nueva_matriz = inicializarMatriz(ancho, alto);
-		
-		for(int i=0; i<_m.size(); i++)
-		for(int j=0; j<_m.get(i).size(); j++)
-			nueva_matriz.get(i).set(j, _m.get(i).get(j));
-		
-		_m = nueva_matriz;
-		
-	}
-	
-	private ArrayList<ArrayList<E>> inicializarMatriz(int ancho, int alto){
-		
-		ArrayList<ArrayList<E>> lista = new ArrayList<ArrayList<E>>();
-		for(int i=0; i<ancho; i++){
-			ArrayList<E> aux = new ArrayList<E>();
-			for(int j=0; j<alto; j++){
-				aux.add(null);
-			}
-			
-			lista.add(aux);
-		}
-		
-		return lista;
-	}
-	
-	private void checkearCoordenada(int i, int j){
-		if( i<0 || i>_m.size() )
-			throw new IllegalArgumentException("Se intento acceder a una posicion invalida de la matriz. fila = " + i);
-		if( j<0 || j>_m.get(0).size() )
-			throw new IllegalArgumentException("Se intento acceder a una posicion invalida de la matriz. columna = " + j);
-	}
-	
-	@Override
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("Matriz:\n");
-		for(ArrayList<E> a:_m)
-			sb.append( a.toString() ).append("\n");
-		return sb.toString();
-	}
+	public V get(K key1, K key2);
+	public boolean set(K key1, K key2, V value);
+	public K rowOf(V e);
+	public K columnOf(V e);
 	
 }

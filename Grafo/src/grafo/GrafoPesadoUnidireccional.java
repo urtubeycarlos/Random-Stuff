6de9863fcc1955679ee0cfe390b5ec36b955fac8
Urtubey Carlos <urtubeycarlos.0510@outlook.com>
2017-05-16 15:55:49 -0300

    Mejorado toString()

--- Grafo/src/grafo/GrafoPesadoUnidireccional.java
+++ Grafo/src/grafo/GrafoPesadoUnidireccional.java
@@ -124,17 +124,32 @@ public class GrafoPesadoUnidireccional<E> extends GrafoUnidireccional<E> {
 		} return ret;
 	}
 	
-	//FIXME: URGENTE! Hacer que el resultado de toString() sea mas lindo.
 	@Override
 	public String toString(){
-		String ret = new String();
-		ret += ("Grafo: {");
+		String ret = new String("Grafo: {");
 		for( E vertice:getVertices() ){
-			ret += vertice.toString() + ": [";
-			for( E vecino:getVecinos(vertice) )
-				ret += vecino.toString() + ", ";
-			ret += ("]; ");
-		} return ret += ("}");
+			ret += vertice.toString() + ": ";
+			ret += getVecinos(vertice).toString() + "; ";
+		} return ret = ret.substring(0, ret.length()-2) + "}";
+	}
+	
+	public static void main(String[] args){
+		GrafoPesadoUnidireccional<Integer> g = new GrafoPesadoUnidireccional<>();
+		g.agregarVertice(1);
+		g.agregarVertice(2);
+		g.agregarVertice(3);
+		g.agregarVertice(4);
+		g.agregarVertice(5);
+		
+		g.agregarArista(1, 2, 5.0);
+		g.agregarArista(2, 3, 5.0);
+		g.agregarArista(1, 3, 7.0);
+		g.agregarArista(4, 5, 7.0);
+
+		System.out.println(g);
+		
+		System.out.println( g.obtenerCaminoMinimo(1, 3) );
+		
 	}
 	
 }
